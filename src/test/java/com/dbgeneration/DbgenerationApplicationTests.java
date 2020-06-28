5d849a538a21d4b2d911978174fb739e78651f26
Trung Duyen <trungduyen0220@gmail.com>
2020-06-28 20:42:30 +0700

    first commit

---
+++ src/test/java/com/dbgeneration/DbgenerationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbgeneration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbgenerationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
