8362c65e935af3ab71b3c7fcd426ed3e562af875
ntmduyen <ntmduyen@cmc.com.vn>
2020-07-01 13:23:21 +0700

    fix

--- src/main/java/com/dbgeneration/controller/HomeController.java
+++ src/main/java/com/dbgeneration/controller/HomeController.java
@@ -1,14 +1,27 @@
 package com.dbgeneration.controller;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.dbgeneration.constants.InputConstants;
+import com.dbgeneration.entity.Table;
+import com.dbgeneration.service.impl.ReadStructureDatabase;
+import com.dbgeneration.utils.ConnectionUtils;
 
 @Controller
 public class HomeController {
 
 	@GetMapping({ "/", "/welcome" })
-	public String welcome(Model model) {
+	public String welcome(Model model) throws SQLException {
+		ConnectionUtils app = new ConnectionUtils();
+		Connection conn = app.connect(InputConstants.JDBC_DRIVER);
+ 
+		List<Table> lstTable = ReadStructureDatabase.readTable(conn, InputConstants.TYPE_OF_DB);
 		return "welcome";
 	}
 }
